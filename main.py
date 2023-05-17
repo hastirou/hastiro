8c82699a6a737835fec0c515e81c84ae4a6c166b
hastirou <112080058+hastirou@users.noreply.github.com>
2023-05-17 01:29:45 +0000

    Modul Praktikum 1: Membuat Aplikasi Web dengan Python Dash

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
-from flask import *
+import dash
+from dash import dcc
+from dash import html
 
-app = Flask(__name__)
+# Membuat instance Dash
+app = dash.Dash(__name__)
+
+# Layout aplikasi
+app.layout = html.Div(children=[
+  html.H1("Selamat Datang di Aplikasi Web Sederhana"),
+  dcc.Input(id="input-text", type="text", placeholder="Masukkan teks"),
+  html.Button("Kirim", id="submit-button", n_clicks=0),
+  html.Div(id="output")
+])
 
 
-@app.route("/")
-def hello_world():
-  return render_template('index.html')
+# Callback untuk merespon input pengguna
+@app.callback(dash.dependencies.Output("output", "children"),
+              [dash.dependencies.Input("submit-button", "n_clicks")],
+              [dash.dependencies.State("input-text", "value")])
+def update_output(n_clicks, input_text):
+  if n_clicks > 0 and input_text:
+    return html.H2(f"Anda mengirim: {input_text}")
+  else:
+    return ""
 
 
+#menjalankan aplikasi
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', debug=True)
+  app.run_server(debug=True)
